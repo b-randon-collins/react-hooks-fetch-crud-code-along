e4ec48b8b9541f45f82e2762ef62bd00dc588f65
brandonc352 <brandonc503@gmail.com>
2024-08-23 14:40:39 -0400

    completed lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,32 +1,73 @@
-import React, { useState } from "react";
+// src/components/ShoppingList.js
+
+import { useEffect, useState } from 'react';
+import Item from "./Item";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
-import Item from "./Item";
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleUpdateItem(updatedItem) {
+    console.log("1ITEMS",items)
+    console.log("1updatedItem",updatedItem)
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    console.log("2ITEMS",items)
+    console.log("2updatedItem",updatedItem)
+
+    setItems(updatedItems);
+    console.log("3ITEMS",items)
+    console.log("3updatedItem",updatedItem)
+
+  }
+  
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
 
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
